b04ee52e3619fbe1ee66f34a31f62a3864fa0b5e
Ayush-Magarde <ayushmagarde03@gmail.com>
2025-12-18 19:58:35 +0530

    [Ayush Magarde] : added a program to find the sum till a given natural number

---
+++ gcr-codebase/control-flows/level-1-practice-programs/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+// Write a program to check for the natural number and write the sum of n natural numbers 
+
+import java.util.Scanner;
+class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+	
+        Scanner input = new Scanner(System.in);
+		// Take a number as an input from the user
+		System.out.print ("Please enter a number: ");
+        int number = input.nextInt();
+        if (number >= 0) {
+            int sum = number * (number + 1) / 2;
+            System.out.println("The sum of " + number + " natural numbers is " + sum);
+        } else {
+            System.out.println("The number " + number + " is not a natural number");
+        }
+        input.close();
+    }
+}
