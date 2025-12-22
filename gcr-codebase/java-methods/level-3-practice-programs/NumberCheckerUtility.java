f51c5ffb3cd445275e2fa768fdff43dc730f46e9
Ayush-Magarde <ayushmagarde03@gmail.com>
2025-12-22 12:48:41 +0530

    [Ayush Magarde] : added a program with various number checking utilities

---
+++ gcr-codebase/java-methods/level-3-practice-programs/NumberCheckerUtility.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+class NumberCheckerUtility {
+	
+	// Method to Find the count of digits in the number
+	public static int digitsCounter (int number) {
+		if (number == 0) return 1;
+		int count = 0;
+		while (number != 0) {
+			count ++;
+			number /= 10;
+		}
+		return count;
+	}
+	
+	// Method to Store the digits of the number in a digits array
+	public static int[] storeDigits (int number, int count) {
+		int [] digitsArray = new int [count];
+		int index = count - 1;
+		while (number != 0) {
+			digitsArray[index--] = number % 10;
+			number /= 10;
+		}
+		return digitsArray;
+	}
+	
+	// Method to Check if a number is a duck number using the digits array
+	public static boolean duckNumberChecker (int[] digitsArray) {
+		if (digitsArray[0] == 0) return false;
+		for (int value : digitsArray) {
+			if (value == 0) return true;
+		}
+		return false;
+	}
+	// Method to check if the number is a armstrong number using the digits array
+	public static boolean armstrongNumberChecker (int[] digitsArray, int number) {
+		int digitSquareSum = 0;
+		for (int value : digitsArray) {
+			digitSquareSum += (int) Math.pow(value, digitsArray.length);
+		}
+		if (number == digitSquareSum) return true;
+		return false;
+	}
+	// Method to find the largest and second largest elements in the digits array
+	public static int[] largestAndSecondLargest (int[] digitsArray) {
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+		for (int value : digitsArray) {
+			if (value > largest) {
+				secondLargest = largest;
+				largest = value;			
+			} else if (value > secondLargest && value != largest) {
+				secondLargest = value;
+			}
+		}
+		return new int[] {largest, secondLargest};
+	}
+	// Method to find the the smallest and second smallest elements in the digits array
+	public static int[] smallestAndSecondSmallest(int[] digitsArray) {
+		int smallest = Integer.MAX_VALUE;
+		int secondSmallest = Integer.MAX_VALUE;
+		for (int value : digitsArray) {
+			if (value < smallest) {
+				secondSmallest = smallest;
+				smallest = value;
+			} 
+			else if (value < secondSmallest && value != smallest) {
+				secondSmallest = value;
+			}
+		}
+		return new int[] {smallest, secondSmallest };
+	}
+}
