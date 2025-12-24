e3a93b0c7d04070fa67d2415258da71a9469f5e5
Ayush-Magarde <ayushmagarde03@gmail.com>
2025-12-24 22:29:00 +0530

    [Ayush Magarde] : added a program to find the maximum of three numbers

---
+++ gcr-codebase/extras/string-built-in/practice-problems-2/MaximumOfThreeNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class MaximumOfThreeNumbers {
+
+    // Method to find maximum of three numbers
+    public static int findMaximum (int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
+
+    // Main method
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int result = findMaximum(a, b, c);
+        System.out.println("Maximum number: " + result);
+    }
+}
