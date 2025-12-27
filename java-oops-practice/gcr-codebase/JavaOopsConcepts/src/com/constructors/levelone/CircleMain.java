b7a308eb2f930c2af2561c033b8ad1fedfc4ad41
Ayush-Magarde <ayushmagarde03@gmail.com>
2025-12-27 22:55:08 +0530

    [Ayush Magarde] : added main class to test circle constructors

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/CircleMain.java
@@ -0,0 +1,12 @@
+package com.constructors.levelone;
+
+public class CircleMain {
+
+	public static void main(String[] args) {
+		Circle circle1 = new Circle();
+		Circle circle2 = new Circle(25);
+		circle1.displayCircleRadius();
+		circle2.displayCircleRadius();
+	}
+
+}
