007fad7bc72113463aefa4d5f727a8f895b12450
Ayush-Magarde <ayushmagarde03@gmail.com>
2025-12-28 13:29:34 +0530

    [Ayush Magarde] : added main class to test book and ebook access control

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/LibraryBookSystemMain.java
@@ -0,0 +1,10 @@
+package com.constructors.levelone;
+
+public class LibraryBookSystemMain {
+
+	public static void main(String[] args) {
+		EBook book1 = new EBook ("12321ABC", "Gone Girl", "Gillian Flynn");
+		book1.displayBookDetails();
+	}
+
+}
