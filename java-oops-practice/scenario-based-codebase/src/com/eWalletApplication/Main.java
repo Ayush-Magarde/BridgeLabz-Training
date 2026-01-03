70dd4cdc6b4984ad7e00f565a05f4e298c930d62
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-03 16:37:34 +0530

    [Ayush Magarde] : implemented E-Wallet application with user registration, wallet types, fund transfers, and transaction history

---
+++ java-oops-practice/scenario-based-codebase/src/com/eWalletApplication/Main.java
@@ -0,0 +1,27 @@
+package com.eWalletApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		User ayush = new User (
+				1, "Ayush", new PersonalWallet(500, true)
+				);
+		
+		User shop = new User (
+				2, "Store", new BusinessWallet(1000));
+				
+		ayush.showBalance();
+		shop.showBalance();
+		
+		ayush.getWallet().transferTo(shop, 300);
+		shop.getWallet().transferTo(ayush, 500);
+		
+		ayush.showBalance();
+		shop.showBalance();
+		
+		ayush.showTransactions();
+		shop.showTransactions();
+		
+	}
+}
