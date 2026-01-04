5e1fd71e7ef72f5c375de323a1d7825f7e85ef5d
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-04 11:13:52 +0530

    [Ayush Magarde] : implemented Hospital Patient Management System using OOP concepts including inheritance, interfaces, and billing logic

---
+++ java-oops-practice/scenario-based-codebase/ScenarioProblems/src/com/day1/hospitalPatientManagementSystem/OutPatient.java
@@ -0,0 +1,15 @@
+package com.day1.hospitalPatientManagementSystem;
+
+public class OutPatient extends Patient {
+
+
+    public OutPatient(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Type: Out-Patient");
+    }
+}
---
+++ java-oops-practice/scenario-based-codebase/ScenarioProblems/src/com/day1/hospitalPatientManagementSystem/Payable.java
@@ -0,0 +1,6 @@
+package com.day1.hospitalPatientManagementSystem;
+
+public interface Payable {
+
+    double calculatePayment();
+}
