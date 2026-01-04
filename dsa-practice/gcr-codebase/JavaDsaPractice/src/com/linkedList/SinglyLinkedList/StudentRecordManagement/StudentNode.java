62c08e5a98f5a37421662d87d3582c795188d8e5
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-04 23:37:54 +0530

    [Ayush Magarde] : implemented student record management using singly linked list

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedList/SinglyLinkedList/StudentRecordManagement/StudentNode.java
@@ -0,0 +1,18 @@
+package com.linkedList.SinglyLinkedList.StudentRecordManagement;
+
+public class StudentNode {
+
+    int roll;
+    String name;
+    int age;
+    char grade;
+    StudentNode next;
+
+    public StudentNode(int roll, String name, int age, char grade) {
+        this.roll = roll;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
