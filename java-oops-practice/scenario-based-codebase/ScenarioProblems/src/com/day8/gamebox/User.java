4496933bc3f30d5bf98750b7f778d1d21216d6c3
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-12 21:05:42 +0530

    [Ayush Magarde] : added gamebox mobile game distribution platform module

---
+++ java-oops-practice/scenario-based-codebase/ScenarioProblems/src/com/day8/gamebox/User.java
@@ -0,0 +1,18 @@
+package com.day8.gamebox;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private String name;
+    private ArrayList<Game> ownedGames;
+
+    public User(String name) {
+        this.name = name;
+        ownedGames = new ArrayList<>();
+    }
+
+    public void addGame(Game game) {
+        ownedGames.add(game);
+    }
+}
