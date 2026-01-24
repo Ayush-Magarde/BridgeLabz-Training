10d77280a3c28b802796940bf397dc269568698a
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-24 21:58:25 +0530

    [Ayush Magarde] : added maxlength annotation for field validation using reflection

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/maxlength/MaxLengthMain.java
@@ -0,0 +1,12 @@
+package com.annotations.maxlength;
+
+public class MaxLengthMain {
+
+    public static void main(String[] args) {
+
+        User user1 = new User("Ayush");        // Valid
+        System.out.println(user1.getUsername());
+
+        User user2 = new User("VeryLongUsername"); // Exception
+    }
+}
