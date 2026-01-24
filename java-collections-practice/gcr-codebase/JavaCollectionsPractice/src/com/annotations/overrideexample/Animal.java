d4ea19b06497f1baeac6fff7cb4fb49e0bf65d82
Ayush-Magarde <ayushmagarde03@gmail.com>
2026-01-24 21:58:58 +0530

    [Ayush Magarde] : added override annotation example using Animal and Dog classes

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/overrideexample/Animal.java
@@ -0,0 +1,8 @@
+package com.annotations.overrideexample;
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/overrideexample/OverrideDemoMain.java
@@ -0,0 +1,10 @@
+package com.annotations.overrideexample;
+
+public class OverrideDemoMain {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Dog();
+        animal.makeSound();
+    }
+}
